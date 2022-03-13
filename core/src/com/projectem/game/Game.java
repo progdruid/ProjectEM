a13d40873b288dd6eb5733c7367d6b10b285c348
Progdruid <progdruid@gmail.com>
2022-03-13 13:55:12 +0200

    some playing with camera, will be deleted after work starts

--- core/src/com/projectem/game/Game.java
+++ core/src/com/projectem/game/Game.java
@@ -4,7 +4,9 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -12,6 +14,8 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class Game extends ApplicationAdapter implements InputProcessor {
 	SpriteBatch batch;
 	Texture img;
+	Sprite sprite;
+	OrthographicCamera camera;
 
 	@Override
 	public void create () {
@@ -19,14 +23,21 @@ public class Game extends ApplicationAdapter implements InputProcessor {
 
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		sprite = new Sprite(img);
+		sprite.setOrigin(0, 0);
+		sprite.setPosition(-sprite.getWidth()/2, - sprite.getHeight()/2);
+		camera = new OrthographicCamera(12800, 7200);
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		//batch.draw(img, 0, 0);
+		sprite.draw(batch);
 		batch.end();
+
 	}
 	
 	@Override
@@ -72,11 +83,15 @@ public class Game extends ApplicationAdapter implements InputProcessor {
 
 	@Override
 	public boolean mouseMoved(int screenX, int screenY) {
+		camera.translate(-(screenX - Gdx.graphics.getWidth() / 2) / 100, (screenY - Gdx.graphics.getHeight() / 2) / 100);
+		camera.update();
 		return false;
 	}
 
 	@Override
 	public boolean scrolled(float amountX, float amountY) {
+		camera.zoom += amountY / 10;
+		camera.update();
 		return false;
 	}
 	//endregion
