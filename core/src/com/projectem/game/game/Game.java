9db5d7d54b0e2bffad67212ab067c9dfa7a12aaa
Progdruid <progdruid@gmail.com>
2022-04-03 00:38:43 +0300

    game class init

---
+++ core/src/com/projectem/game/game/Game.java
@@ -0,0 +1,20 @@
+package com.projectem.game.game;
+
+import com.projectem.game.input.*;
+import com.projectem.game.ui.*;
+
+public class Game implements IUIAcceptor, ICommonInputAcceptor {
+
+    IUIManager uiManager;
+    IPlatformInput inputManager;
+
+    public Game (IUIManagerCreator uiCreator, IPlatformInputCreator inputCreator) {
+        uiManager = uiCreator.createUIManager(this);
+        inputManager = inputCreator.createInputManager(this);
+    }
+
+    public void update () {
+
+    }
+
+}
