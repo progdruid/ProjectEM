72170839393055212149097cc59f3bdf324a09cf
Progdruid <progdruid@gmail.com>
2022-04-17 19:08:03 +0300

    entity children feature

--- core/src/com/projectem/game/ecs/Entity.java
+++ core/src/com/projectem/game/ecs/Entity.java
@@ -1,19 +1,40 @@
 package com.projectem.game.ecs;
 
 import com.badlogic.gdx.utils.Disposable;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Entity implements Disposable {
 
     public String name = "";
+
+    private List<Entity> children;
+    public Entity parent;
+
     public HashMap<String, IComponent> components;
     public TransformComponent transform;
 
     public Entity (String name) {
         EntityGod.ins.entities.add(this);
         this.name = name;
+        this.children = new ArrayList<>();
         this.components = new HashMap<>();
-        transform = (TransformComponent) TransformSystem.ins.createComponent(this);
+        this.transform = (TransformComponent) TransformSystem.ins.createComponent(this);
+    }
+
+    public int getChildrenCount () {
+        return children.size();
+    }
+
+    public Entity getChild (int index) {
+        return children.get(index);
+    }
+
+    public void addChild (Entity child) {
+        children.add(child);
+        child.parent = this;
     }
 
     public void dispose (){
